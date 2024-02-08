07035bbfbbd20ad32e74212e7667f692e96bf97c
Mateus Sousa <mateuss4570@gmail.com>
2024-02-08 19:56:27 -0300

    updating the requeriments of the input

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from gfpgan import GFPGANer
 import cv2
 import os
 import numpy as np
+
 app = FastAPI()
 
-# MongoDB Settings - to test [change the database if needed]
+# MongoDB Settings
 MONGO_URL = "mongodb://localhost:27017"
 DATABASE_NAME = "mine"
 COLLECTION_NAME = "teste_gan"
@@ -23,31 +24,21 @@ db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
 
 # Set up GFPGAN restorer globally
-# You may want to use dependency injection or a singleton pattern for better performance in a production environment
 restorer = GFPGANer(
     model_path="GFPGANv1.3.pth",
     #model_path="GFPGANCleanv1-NoCE-C2.pth",
-    # ! ANY OTHER .pth file
-    upscale=2,
-    arch="clean",
-    channel_multiplier=2,
-    bg_upsampler=None  # Set your background upsampler here
+    #upscale=2,4,8...
+    #arch=clean | original
+    #channel_multiplier=2,
+    #bg_upsampler=None, # Set your background upsampler here
+    only_center_face: bool = True 
 )
 
 
 @app.post("/process_image")
 async def process_image(
-    input_file: UploadFile = File(...),
-    output_folder: str = "outputs",
-    version: str = "1.3",
-    upscale: int = 2,
-    bg_upsampler: str = "realesrgan",
-    bg_tile: int = 400,
-    suffix: str = None,
-    only_center_face: bool = False,
-    aligned: bool = False,
-    ext: str = "auto",
-    weight: float = 2,
+    alarm: UploadFile = File(...),
+    bg_upsampler: str = "realesrgan"
 ):
     try:
         contents = await input_file.read()
@@ -63,7 +54,7 @@ async def process_image(
             weight=weight,
         )
 
-        # Create a temporary directory
+        # Create a temporary directory, just locally to see the output
         with tempfile.TemporaryDirectory() as temp_dir:
             # Save the upscaled image in the temporary directory
             output_path = os.path.join(temp_dir, f"{input_file.filename}_upscaled_better.png")
@@ -73,11 +64,10 @@ async def process_image(
             # Save results to MongoDB
             '''result_data = {
                 "input_file_name": input_file.filename,
-                "output_folder": output_folder,
-                "version": version
+                "output_folder": output_folder
             }'''
             #await collection.insert_one(result_data)
-            
+
             # Move the saved image to the specified output folder
             output_path_final = os.path.join(output_folder, os.path.basename(output_path))
             shutil.move(output_path, output_path_final)
@@ -87,19 +77,19 @@ async def process_image(
 
                 "input_file_name": input_file.filename,
                 "output_folder": output_folder,
-                "version": version,
                 "encoded_image": img_base64
-                # Add more fields as needed
+                # Add more fields... or remove...
             }
             await collection.insert_one(dict(result_json))
 
             return JSONResponse(content=jsonable_encoder(result_json), status_code=200)
-    
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+    
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}, the image has not been upscayled")
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
